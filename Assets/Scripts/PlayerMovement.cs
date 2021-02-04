2b6c4bc9da50526fb9ef5b5f88a90bd3e8e72ba3
Samuel Walker <ldspcjunkie@gmail.com>
2021-02-03 21:21:10 -0700

    Implemented basic player movement.

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+    public float playerSpeed = 5;
+
+    void Start() {
+
+    }
+
+    void Update() {
+        Vector3 movement = Vector3.zero;
+        if(Input.GetAxis("Vertical") > 0) {
+            movement.y = 1;
+        }else if(Input.GetAxis("Vertical") < 0) {
+            movement.y = -1;
+        }
+
+        if(Input.GetAxis("Horizontal") > 0) {
+            movement.x = 1;
+        }else if(Input.GetAxis("Horizontal") < 0) {
+            movement.x = -1;
+        }
+
+        transform.Translate(movement * playerSpeed * Time.deltaTime);
+        
+    }
+}
