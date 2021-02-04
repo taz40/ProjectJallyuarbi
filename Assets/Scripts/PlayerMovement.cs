af81a011dad4f82e1ea24fd66773fdfd710b6a32
Samuel Walker <ldspcjunkie@gmail.com>
2021-02-04 15:39:50 -0700

    Added collision detection. (still a bit buggy)

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,27 +4,28 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour {
 
-    public float playerSpeed = 5;
+    public float playerSpeed = 20.0f;
+
+    Rigidbody2D body;
+    float horizontal;
+    float vertical;
+    float moveLimiter = 1f;
 
     void Start() {
-
+        body = GetComponent<Rigidbody2D>();
     }
 
     void Update() {
-        Vector3 movement = Vector3.zero;
-        if(Input.GetAxis("Vertical") > 0) {
-            movement.y = 1;
-        }else if(Input.GetAxis("Vertical") < 0) {
-            movement.y = -1;
-        }
 
-        if(Input.GetAxis("Horizontal") > 0) {
-            movement.x = 1;
-        }else if(Input.GetAxis("Horizontal") < 0) {
-            movement.x = -1;
-        }
-
-        transform.Translate(movement * playerSpeed * Time.deltaTime);
+        horizontal = Input.GetAxisRaw("Horizontal");
+        vertical = Input.GetAxisRaw("Vertical");
         
+    }
+
+    private void FixedUpdate() {
+
+        Vector2 dir = new Vector2(horizontal, vertical).normalized;
+
+        body.velocity = dir * playerSpeed;
     }
 }
