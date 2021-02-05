39d7e031472099ca0a86b26c6e89710e3f55f5f6
Samuel Walker <ldspcjunkie@gmail.com>
2021-02-04 17:33:23 -0700

    Implemented basid dialog controller

---
+++ Assets/Scripts/DialogControler.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Ink.Runtime;
+using UnityEngine.UI;
+
+public class DialogControler : MonoBehaviour {
+
+    public TextAsset story;
+    Story ink;
+    public Text text;
+    public GameObject button;
+    public GameObject buttonList;
+
+    // Start is called before the first frame update
+    void Start() {
+        ink = new Story(story.text);
+        showLine();
+    }
+
+    // Update is called once per frame
+    void Update() {
+        if (Input.GetButtonDown("Continue") && ink.currentChoices.Count == 0) {
+            showLine();
+        }
+    }
+
+    private void showLine() {
+        if (ink.canContinue) {
+            text.text = ink.Continue();
+        }
+
+        if(ink.currentChoices.Count > 0) {
+            showChoices();
+        }
+    }
+
+    private void showChoices() {
+        for(int i = 0; i < ink.currentChoices.Count; i++) {
+            GameObject go = Instantiate(button, buttonList.transform);
+            go.GetComponentInChildren<Text>().text = ink.currentChoices[i].text;
+            int index = i;
+            go.GetComponent<Button>().onClick.AddListener(() => { choose(index); });
+        }
+    }
+
+    private void choose(int index) {
+        for(int i = 0; i < buttonList.transform.childCount; i++) {
+            Destroy(buttonList.transform.GetChild(i).gameObject);
+        }
+        ink.ChooseChoiceIndex(index);
+        showLine();
+    }
+}
