f1cd44e41472e9b480d3d8be2b5a0f90c25d90f1
Samuel Walker <ldspcjunkie@gmail.com>
2021-02-20 14:02:50 -0700

    Added comments.

--- Assets/Scripts/DialogControler.cs
+++ Assets/Scripts/DialogControler.cs
@@ -13,22 +13,23 @@ public class DialogControler : MonoBehaviour {
     public GameObject button;
     public GameObject buttonList;
 
-    // Start is called before the first frame update
     void Start() {
+        //Load the story, set variables, get speeker name and show the first line of dialog
         ink = new Story(story.text);
         ink.variablesState["players_name"] = "Bob";
         name.text = ink.variablesState["speeker"].ToString();
         showLine();
     }
 
-    // Update is called once per frame
     void Update() {
+        //If someone presses the continue button and there is not choice to make, show the next line
         if (Input.GetButtonDown("Continue") && ink.currentChoices.Count == 0) {
             showLine();
         }
     }
     
     private void showLine() {
+        //Show the line on the screen if there is not a choice to be made.
         if (ink.canContinue) {
             text.text = ink.Continue();
             if (ink.currentTags.Contains("end")) {
@@ -40,12 +41,14 @@ public class DialogControler : MonoBehaviour {
             }
         }
 
+        //If there is a choice to be made, show the choice to the player.
         if(ink.currentChoices.Count > 0) {
             showChoices();
         }
     }
 
     private void showChoices() {
+        //make a new button object for each option the player has, and set its callback to make that choice.
         for(int i = 0; i < ink.currentChoices.Count; i++) {
             GameObject go = Instantiate(button, buttonList.transform);
             go.GetComponentInChildren<Text>().text = ink.currentChoices[i].text;
@@ -55,6 +58,7 @@ public class DialogControler : MonoBehaviour {
     }
 
     private void choose(int index) {
+        //Destroy all button objects and tell ink to choose the choice, then show the next line.
         for(int i = 0; i < buttonList.transform.childCount; i++) {
             Destroy(buttonList.transform.GetChild(i).gameObject);
         }
