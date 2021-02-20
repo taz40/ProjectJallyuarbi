4eab2644b05a89d23a4851a253b90786063ce5a1
Jack <delstrom2@outlook.com>
2021-02-20 13:50:58 -0800

    Added basic enemy movement.

---
+++ Assets/Scripts/EnemyAiTest.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyAiTest : EnemyBasic
+{
+
+    private Rigidbody2D myRigidbody;
+    public Transform target;
+    public float chaseRadius;
+    public float attackRadius;
+    public float wakeRadius;
+    public Transform homePosition;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentState = EnemyState.idle;
+        myRigidbody = GetComponent<Rigidbody2D>();
+        target = GameObject.FindWithTag("Player").transform;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        CheckDistance();
+    }
+
+    void CheckDistance()
+    {
+        if (Vector3.Distance(target.position,
+                            transform.position) <= chaseRadius
+                                && Vector3.Distance(target.position,
+                                                    transform.position) > attackRadius)
+        {
+            if (currentState == EnemyState.idle || currentState == EnemyState.walk)
+            {
+                Vector3 temp = Vector3.MoveTowards(transform.position, target.position, moveSpeed * Time.deltaTime);
+
+                myRigidbody.MovePosition(temp);
+                ChangeState(EnemyState.walk);
+            }
+        }
+    }
+
+    private void ChangeState(EnemyState newState)
+    {
+        if (currentState != newState)
+        {
+            currentState = newState;
+        }
+    }
+}
