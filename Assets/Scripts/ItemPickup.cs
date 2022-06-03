f725ff47afa82f5adbfe9ffc4a086cf931f25cc0
Samuel Walker <ldspcjunkie@gmail.com>
2022-06-03 17:24:48 -0600

    Dynamic item sprite loading.

--- Assets/Scripts/ItemPickup.cs
+++ Assets/Scripts/ItemPickup.cs
@@ -5,10 +5,14 @@ using UnityEngine;
 public class ItemPickup : Interactable {
 
     public ItemStack item;
+    Sprite tex;
 
     // Start is called before the first frame update
     void Start() {
         item = new ItemStack(Items.ITEM_TWIG, 10);
+        tex = Resources.Load<Sprite>("Sprites/"+item.getItem().getName().Replace(":", "/"));
+        Debug.Log("Sprites/"+item.getItem().getName().Replace(":", "/"));
+        GetComponentInChildren<SpriteRenderer>().sprite = tex;
     }
 
     // Update is called once per frame
