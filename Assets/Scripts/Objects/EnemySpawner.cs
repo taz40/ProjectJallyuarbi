aa0f5c5caf546436ba933bf082c092649d2a4fda
Samuel Walker <swalker@piwalker.net>
2023-08-14 22:00:06 -0600

    Programmed basic entity spawner. and added goblin prefab.

--- Assets/Scripts/Objects/EnemySpawner.cs
+++ Assets/Scripts/Objects/EnemySpawner.cs
@@ -2,17 +2,30 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class EnemySpawner : MonoBehaviour
-{
+public class EnemySpawner : MonoBehaviour {
+
+    public string entityType;
+    private GameObject prefab;
+
     // Start is called before the first frame update
     void Start(){
 
-
+        prefab = Resources.Load<GameObject>("Prefabs/Entities/"+entityType);
+        if(prefab == null) {
+            Debug.LogError("Entity " + entityType + " does not exist.");
+            Destroy(gameObject);
+        } else {
+            summon();
+        }
         
     }
 
     // Update is called once per frame
     void Update(){
         
+    }
+
+    void summon() {
+        Instantiate(prefab, transform.position, transform.rotation, transform);
     }
 }
